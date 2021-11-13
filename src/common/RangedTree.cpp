05e9c9f9647421709dcc5410591825e0ffb1559e
Guy Marino <gxm262@case.edu>
2021-11-13 18:37:06 -0500

    Add copy/move implementation

--- src/common/RangedTree.cpp
+++ src/common/RangedTree.cpp
@@ -30,3 +30,45 @@ _node_type(node_type),
 _value(value) { }
 
 
+template <class T>
+RangedTree<T>::RangedTreeNode::RangedTreeNode(const RangedTreeNode& other):
+_node_type(other._node_type),
+_value(other._value),
+_tree_height(other._tree_height),
+_parent(other._parent)              // Preserve the parent. Worst case it's unreachable
+{
+    if( other._left_child )
+    {
+        _left_child = new RangedTreeNode(*other._left_child);
+    }
+
+    if( other._right_child )
+    {
+        _right_child = new RangedTreeNode(*other._right_child);
+    }
+}
+
+
+template <class T>
+RangedTree<T>::RangedTreeNode::RangedTreeNode(RangedTreeNode&& other) noexcept:
+_node_type(other._node_type),
+_value(other._value),
+_tree_height(other.tree_height),
+_parent(other._parent)              // Preserve the parent. Worst case it's unreachable
+{
+    // Make sure no self-assignment
+    if( this == *other )
+    {
+        return;
+    }
+
+    _left_child = other._left_child;
+    other._left_child = nullptr;
+
+    _right_child = other._right_child;
+    other._right_child = nullptr;
+}
+
+
+template struct xregex::common::RangedTree<char>;
+template struct xregex::common::RangedTree<wchar_t>;
